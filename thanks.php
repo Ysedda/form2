61b62cf2ab3b89e2ae9e0052d42ff772a929c1fd
Ysedda <arthur.baudoin@icloud.com>
2022-03-21 15:51:44 +0100

    done

---
+++ thanks.php
@@ -0,0 +1,7 @@
+
+
+Merci <?php echo $_POST['user_first_name'] . $_POST['user_name']; ?> de nous avoir contacté à propos de <?php echo $_POST['subjects']?>.
+
+Un de nos conseiller vous contactera soit à l'adresse <?php echo $_POST['user_email']?> ou par téléphone au <?php echo $_POST['user_phone']?> dans les plus brefs délais pour traiter votre demande : 
+
+<?php echo $_POST['user_message'] ?>
