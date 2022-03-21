65a52dc2b55fda78bc55ffd423eb6d3439fc8517
Ysedda <arthur.baudoin@icloud.com>
2022-03-21 15:57:51 +0100

    fixed html in thanks.php

--- thanks.php
+++ thanks.php
@@ -1,7 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-Merci <?php echo $_POST['user_first_name'] . $_POST['user_name']; ?> de nous avoir contacté à propos de <?php echo $_POST['subjects']?>.
+<body>
 
-Un de nos conseiller vous contactera soit à l'adresse <?php echo $_POST['user_email']?> ou par téléphone au <?php echo $_POST['user_phone']?> dans les plus brefs délais pour traiter votre demande : 
+    <p> Merci <?php echo $_POST['user_first_name'] . $_POST['user_name']; ?> de nous avoir contacté à propos de <?php echo $_POST['subjects'] ?>. </br>
 
-<?php echo $_POST['user_message'] ?>
+        Un de nos conseiller vous contactera soit à l'adresse <?php echo $_POST['user_email'] ?> ou par téléphone au <?php echo $_POST['user_phone'] ?> dans les plus brefs délais pour traiter votre demande : </br>
+
+        <?php echo $_POST['user_message'] ?> </p>
+</body>
+
+</html>
